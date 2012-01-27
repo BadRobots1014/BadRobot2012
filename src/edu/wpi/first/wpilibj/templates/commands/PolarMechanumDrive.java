f8fefd24b3f6bc313681300b619587026b21cc95
Jon Buckley <jonbuckley33@gmail.com>
2012-01-26 19:57:20 -0500

    Polar Mechanum Drive added

---
+++ src/edu/wpi/first/wpilibj/templates/commands/PolarMechanumDrive.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import com.sun.squawk.util.MathUtils;
+import edu.wpi.first.wpilibj.templates.OI;
+
+/**
+ *
+ * @author Jon Buckley
+ */
+public class PolarMechanumDrive extends CommandBase {
+    
+    public PolarMechanumDrive() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    /*
+     * Status: untested, faulty
+     */
+    protected void execute() 
+    {
+        double mag = Math.sqrt((OI.getRightX()*OI.getRightX()) + (OI.getRightY()*OI.getRightY()));
+        double angle = MathUtils.atan(OI.getRightY()/OI.getRightX()) * (180/3.14159);
+        
+        System.out.println("Mag = " + mag + "  Angle = " + angle + " OI Right y " + OI.getRightY());
+        
+        driveTrain.polarMechanum(mag, angle, .2);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
