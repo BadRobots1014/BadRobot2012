d46ecf6956c8ed29af65cc93c56ec85236a84b5c
Jon Buckley <jonbuckley33@gmail.com>
2012-02-01 15:17:19 -0500

    really includes reset gyro

---
+++ src/com/badrobots/y2012/technetium/buttons/ResetGyro.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.badrobots.y2012.technetium.buttons;
+
+import com.badrobots.y2012.technetium.OI;
+import com.badrobots.y2012.technetium.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+/**
+ *
+ * @author Jon Buckley
+ */
+public class ResetGyro extends Button
+{
+
+    public boolean get() 
+    {
+        if (OI.leftJoystick.getRawButton(4) && OI.rightJoystick.getRawButton(4))
+        {
+            DriveTrain.getInstance().resetGyro();
+            return true;
+        }
+        
+        return false;
+    }
+    
+}
