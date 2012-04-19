5eab8d7919c04da41b6f9ce924d7f80dbf099f9f
Jon Buckley <jonbuckley33@gmail.com>
2012-04-19 08:45:08 -0400

    Debugging code -- no luck. DigitalInput constantly returns true.

--- src/com/badrobots/y2012/technetium/subsystems/GearToothPID.java
+++ src/com/badrobots/y2012/technetium/subsystems/GearToothPID.java
@@ -4,7 +4,6 @@
  */
 package com.badrobots.y2012.technetium.subsystems;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.GearTooth;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -16,60 +15,71 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class GearToothPID extends Thread implements PIDSource
 {
+
     protected GearTooth gearTooth;
     protected boolean running = true;
+    protected boolean stopped = false;
     protected double gearToothSpeed = -1;
     protected double gearToothPeriod = 0;
     protected int sleepTime = 50;
+    protected boolean inChannel = false;
     protected DigitalInput in;
-    
+
     public GearToothPID(int port)
     {
-        gearTooth = new GearTooth(port, false);
-        //in = new DigitalInput(port);
+        //gearTooth = new GearTooth(port, false);
+        in = new DigitalInput(port);
         running = true;
-        SmartDashboard.putDouble("GearToothSpeed", gearToothSpeed);
-        gearTooth.reset();
+        /*gearTooth.reset();
         gearTooth.setMaxPeriod(2);
         gearTooth.start();
+
+        gearToothSpeed = gearTooth.get();
+        stopped = gearTooth.getStopped();*/
+        
+        inChannel = in.get();
+        
+        SmartDashboard.putBoolean("DigitalInput", inChannel);
+       /// SmartDashboard.putBoolean("GearToothStopped", stopped);
     }
-    
+
     public void run()
     {
-        while(true)
+        while (true)
         {
             //System.out.println("running outside run method loop");
             if (true)//if this is "running", the code breaks. Why?
-            {   
-               // System.out.println("running run method in GearToothPID");
-                try
-                {
-                    Thread.sleep(sleepTime);
-                }
-                catch (InterruptedException ex)
-                {
-                    ex.printStackTrace();
-                }
-                
-                
+            {
+                // System.out.println("running run method in GearToothPID");
+
                 //gearToothPeriod = gearTooth.getPeriod();
-              //  gearToothSpeed = gearTooth.get();
-                System.out.println("GearToothSpeed ");
-                SmartDashboard.putDouble("GearToothSpeed", gearToothSpeed);
+                /*gearToothSpeed = gearTooth.get();
+                stopped = gearTooth.getStopped();
+                System.out.println("GearToothCount " + gearToothSpeed);*/
+                
+                inChannel = in.get();
+                SmartDashboard.putBoolean("DigitalInput", inChannel);
                 //SmartDashboard.putDouble("GearToothPeriod", gearToothPeriod);
+            }
+
+            try
+            {
+                Thread.sleep(sleepTime);
+            }
+            catch (InterruptedException ex)
+            {
+                ex.printStackTrace();
             }
         }
     }
 
     public double pidGet()
     {
-        return gearToothSpeed;   
+        return gearToothSpeed;
     }
 
     public void setRunning(boolean b)
     {
         running = b;
     }
-    
-    
 }
